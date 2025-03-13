bbb76bdf1312aab5c9ee8d95e5a75c5c1efa9f41
TeoGW <guangweiteo@gmail.com>
2025-03-13 16:50:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ self.addEventListener('install', (event) => {
         .catch(err => console.log("SW Registration Failed", err));
     }
 
-
+let response = await fetch("./products.json");
 document.addEventListener("DOMContentLoaded", function () {
     async function fetchProducts() {
     try {
